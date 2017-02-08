d60d11f707452c029daf8732ba84c806aa4fdcb1
WangwangOK <wwloading@163.com>
2017-02-08 21:31:10 +0800

    Initial Commit

---
+++ MOOC_C++Programing/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MOOC_C++Programing
+//
+//  Created by 王望 on 2017/2/8.
+//  Copyright © 2017年 Will. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
